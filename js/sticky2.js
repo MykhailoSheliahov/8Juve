8dfd23090a5d253d295380879b413aeb3b13e725
Mihail <30924289+ShelyagovMV@users.noreply.github.com>
2019-02-03 19:51:43 +0200

    Juve fan site

---
+++ js/sticky2.js
@@ -0,0 +1,22 @@
+
+var header = document.querySelector('#nav');
+var header2 = document.querySelector('#headerLogoMin');
+console.log(header);
+var doc = document.documentElement || document.scrollingElement;//for safari mozilla
+var sticky = 250;
+var loader = document.querySelector('#loader');
+setTimeout(function () {
+    doc.scrollTop = 0;
+    loader.style.display = 'none';
+    window.addEventListener('scroll',function(){
+        if(doc.scrollTop > sticky){
+            header.classList.add('sticky');
+            header2.style.display = 'block';
+
+        }
+        else{
+            header.classList.remove('sticky');
+        }
+    });
+
+},1000);
